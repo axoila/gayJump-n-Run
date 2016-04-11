6d1a2ef8c8c04099b18606563b4cbac38b764f1e
Ronja <axoila@gmx.de>
2016-04-11 23:18:39 +0200

    renamed js file, put all controlls in controlls folder in the future

--- controls.js
+++ controls.js
@@ -10,15 +10,15 @@
 
 // The keycodes that will be mapped when a user presses a button.
 KEY_CODES = {
-  32: 'space',
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  77: 'm',
-  78: 'n',
-  89: 'y',
-  82: 'r',
+    32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    77: 'm',
+    78: 'n',
+    89: 'y',
+    82: 'r',
 }
 
 /* Creates the array to hold the KEY_CODES and sets all their values
@@ -30,13 +30,13 @@ for (code in KEY_CODES) {
 KEY_STATUS[ KEY_CODES[ code ]] = false; }
 	/** * Sets up the document to listen to onkeydown events
 	(fired when * any key on the keyboard is pressed down).
-	 When a key is pressed, * it sets the appropriate direction
-	 to true to let us know which * key it was. */
+	When a key is pressed, * it sets the appropriate direction
+	to true to let us know which * key it was. */
 document.onkeydown = function(e) {
-	 // Firefox and opera use charCode instead of keyCode to
-	 // return which key was pressed.
-	 var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-	 if (KEY_CODES[keyCode]) {
+	// Firefox and opera use charCode instead of keyCode to
+	// return which key was pressed.
+	var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+	if (KEY_CODES[keyCode]) {
 		e.preventDefault();
 		KEY_STATUS[KEY_CODES[keyCode]] = true;
 	}
@@ -45,10 +45,10 @@ document.onkeydown = function(e) {
 	 (fired when * any key on the keyboard is released). When a key
 	 is released, * it sets teh appropriate direction to false to let
 	 us know which * key it was. */
- document.onkeyup = function(e) {
-	 var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-	 if (KEY_CODES[keyCode]) {
-			 e.preventDefault();
-			 KEY_STATUS[KEY_CODES[keyCode]] = false;
-		 }
+document.onkeyup = function(e) {
+	var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+	if (KEY_CODES[keyCode]) {
+			e.preventDefault();
+			KEY_STATUS[KEY_CODES[keyCode]] = false;
+		}
 }
