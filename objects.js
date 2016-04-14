c0a87163b813631edf8fdd52d62272aeee13d943
Ronja <axoila@users.noreply.github.com>
2016-04-14 10:21:31 +0200

    derping around because I'm bored and android class is heöö

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
 //file for all classes with Objects like players, platforms, enemies, everything
+
+function Hero(){
+  this.hp = 20;
+  this.damage = 5;
+  this.weapon = 1; //1 is spear; 2 is 1H sword
+}
+Hero.prototype = new PhysicsObject();
